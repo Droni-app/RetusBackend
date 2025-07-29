925ff61b7c6f6ff7170b3d8a21e3fe059e72ff50
Gustavo Barragan <dev@droni.co>
2025-07-28 21:57:47 -0500

    chore: simplify auth

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -4,33 +4,33 @@ import User from '#models/user'
 
 export default class AuthMiddleware {
   async handle(ctx: HttpContext, next: NextFn) {
-    /**
-     * Middleware logic goes here (before the next call)
-     */
     const token = String(ctx.request.header('Authorization')).replace('Bearer ', '')
-    const allyInfo = await ctx.ally.use('google').userFromToken(token)
-    console.log('Ally Info:', allyInfo)
 
-    if (!allyInfo) {
-      ctx.response.status(401).send({ error: 'Unauthorized' })
-      return
-    }
-    const user = await User.firstOrCreate(
-      { providerId: allyInfo.id, provider: 'google' },
-      {
-        email: allyInfo.email,
-        name: allyInfo.name,
-        avatar: allyInfo.avatarUrl,
-        role: 'user',
-        rank: 0,
-        points: 0,
+    try {
+      const allyInfo = await ctx.ally.use('google').userFromToken(token)
+      console.log('Ally Info:', allyInfo)
+
+      if (!allyInfo) {
+        return ctx.response.status(401).send({ error: 'Unauthorized' })
       }
-    )
-    console.log('Authenticated User:', user)
-    ctx.auth.user = user
-    /**
-     * Call next method in the pipeline and return its output
-     */
+
+      const user = await User.firstOrCreate(
+        { providerId: allyInfo.id, provider: 'google' },
+        {
+          email: allyInfo.email,
+          name: allyInfo.name,
+          avatar: allyInfo.avatarUrl,
+          role: 'user',
+          rank: 0,
+          points: 0,
+        }
+      )
+
+      console.log('Authenticated User:', user)
+    } catch (error) {
+      return ctx.response.status(401).send({ error: 'Unauthorized' })
+    }
+
     const output = await next()
     return output
   }
